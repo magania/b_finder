034f5a219d80ee4d5826116f8ffbe3bc69abb3e7
Ricardo <magania@gmail.com>
2009-07-22 14:45:20 -0500

    Modified a2_bd to run over the jpsi lists.

--- go_cab.sh
+++ go_cab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for t in `seq 1 250`; do 
-/usr/bin/qsub -q medium@d0cabsrv1 -l nodes=1 -k oe -m ae a2_jpsi.sh 
-/usr/bin/qsub -q medium@d0cabsrv2 -l nodes=1 -k oe -m ae a2_jpsi.sh 
+for t in `seq 1 200`; do 
+/usr/bin/qsub -q medium@d0cabsrv1 -l nodes=1 -k oe -m ae a2_bd.sh 
+/usr/bin/qsub -q medium@d0cabsrv2 -l nodes=1 -k oe -m ae a2_bd.sh 
 done
